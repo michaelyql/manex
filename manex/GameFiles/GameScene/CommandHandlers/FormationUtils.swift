68dc87282701bfcd2100dec6866191a47aa14fbb
michaelyql <yql.michael@gmail.com>
2023-06-04 00:50:33 +0800

    Duplicated getCurrentShipsPosition() and convertShipCoordsToPolarPoints() functions from GameScene class over to FormationUtils class

--- manex/GameFiles/GameScene/CommandHandlers/FormationUtils.swift
+++ manex/GameFiles/GameScene/CommandHandlers/FormationUtils.swift
@@ -12,8 +12,51 @@ import SpriteKit
 
 class FormationUtils {
     
-    func getCurrentShipsPosition() {
+    static func getCurrentShipsPosition(warships: [Warship]) -> [CGPoint] {
+        var pts: [CGPoint] = []
+        for ship in warships {
+            pts.append(ship.position)
+        }
+        return pts
+    }
+    
+    static func convertShipCoordsToPolarPoints(warships: [Warship]) -> [PolarPoint] {
+        var p: [PolarPoint] = []
+        guard warships.count > 0 else { return p }
         
+        // first ship is the origin that every other ship is referenced from
+        let originX = warships[0].position.x
+        let originY = warships[0].position.y
+        
+        for ship in warships {
+            if ship == warships[0] {
+                p.append(PolarPoint(r: 0, a: 0))
+                continue
+            }
+            
+            // calculate ship's distance from origin
+            let distance = hypot((ship.position.x - originX), (ship.position.y - originY))
+            
+            // calcuate ship's angle of rotation from origin (w.r.t Y-axis)
+            let trueAngle: CGFloat = -atan2((ship.position.y - originY), ship.position.x - originX) + .pi / 2
+            
+            let relativeAngle = trueAngle + ship.zRotation < 0
+            ? (trueAngle + ship.zRotation + .pi * 2)
+            : (trueAngle + ship.zRotation)
+            
+            p.append(PolarPoint(r: distance, a: relativeAngle))
+        }
+        return p
+        
+        /* CALCULATIONS EXPLAINED
+         atan2 is used to get angle in all 4 quadrants.
+         since atan2 measures angles w.r.t X axis, .pi/2 needs to be added so that angles are w.r.t Y axis
+         we need positive angle to represent clockwise rotation but for atan2 positive angle means
+         counterclockwise rotation. hence we need to negate atan2
+         
+         finally, relativeAngle = trueAngle - (-ship.zRotation)
+         thus relativeAngle = trueAngle + ship.zRotation
+         */
     }
     
     func generatePositions() {
